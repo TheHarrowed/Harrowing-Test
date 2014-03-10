8c068321d7779d17aa68bd756e79bd06e2607531
Michael <xmal1ciousx@hotmail.com>
2014-03-10 09:45:23 -0400

    Fixed Javascipt code, will now edit HTML properly

--- js/myscript.js
+++ js/myscript.js
@@ -1,28 +1,11 @@
-var userName = "HarroweD";
-var curHP = 10;
-var maxHP = 10;
+var curExp = 0;
+var expNeeded;
 var gold = 0;
 var str = 5;
-var exp = 0.0;
-var expNeeded;
-var playerLevel;
+var curHP, maxHP = 10;
 
-function pageLoad(){
-	document.getElementById("userName").innerHTML=<b>"HarroweD"</b>;
-	prompt("Fuck!");
-	alert("Fuck!");
-}
-
-function submitAction(){
-	prompt("You killed the Naked Negro!");
-	exp+=.05;
-	if(exp>=1.0){
-		playerLevel++;
-		exp=0;
-	}
-}
-
-function getExp(){
-	document.write(5);
-	return exp;	
+function submitPress(){
+	alert("Poop!");
+	curExp+=5;
+	document.getElementById("exp").innerHTML="Experience: " + curExp;
 }
