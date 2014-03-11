936fe373b2686b840e3a675e891953325093a748
Michael <xmal1ciousx@hotmail.com>
2014-03-11 09:27:34 -0400

    Players names now display correctly over their picture placeholders

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,25 @@
+var userName = "HarroweD";
+var pooName = "PoooooPe";
 var curExp = 0;
 var expNeeded;
 var gold = 0;
 var str = 5;
 var curHP, maxHP = 10;
 
+
+
+
+function onPageLoad(){
+}
+
 function submitPress(){
-	alert("Poop!");
+	alert("Fight!!");
 	curExp+=5;
-	document.getElementById("exp").innerHTML="Experience: " + curExp;
 	
+	var x = document.getElementById("mobSelect").selectedIndex;
+	var y = document.getElementById("mobSelect").options;
+	
+	document.getElementById("playerName").innerHTML= userName;
+	document.getElementById("mobName").innerHTML= y[x].text;
+
 }
